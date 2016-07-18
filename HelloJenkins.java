1d4441ab40012e594d611c93a4f5f1c2cf3737ed
chetanagrawal <caagrawal@gmail.com>
2016-07-18 10:41:56 +0530

    Create HelloJenkins.java
    Sample java code for Jenkins demo

---
+++ HelloJenkins.java
@@ -0,0 +1,6 @@
+public class HelloJenkins {
+   public static void main(String[] args) {
+      // Prints "Hello, Jenkins" in the terminal window.
+      System.out.println("Hello, Jenkins!");
+   }
+}
